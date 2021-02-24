9df32a2e0255621df55af9bdcc8c568fa7b55a16
Erik Huang <erikhuangdrums@gmail.com>
2021-02-24 00:27:23 -0800

    reviewed

--- src/index.js
+++ src/index.js
@@ -1,1 +1,55 @@
-console.log('%c HI', 'color: firebrick')
+const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+const breedUrl = 'https://dog.ceo/api/breeds/list/all'
+const ul = document.querySelector('#dog-breeds')
+const container = document.querySelector('#dog-image-container')
+const select = document.querySelector('#breed-dropdown')
+
+const setImgs = data => {
+    for (let msg of data) {
+        const image = document.createElement('img')
+        image.src = msg
+        image.style.width = "200px"
+        container.appendChild(image)
+    }
+}
+
+const setLis = (data, val=null) => {
+    for (let msg in data) {
+        if (!val || msg[0] === val) {
+            const li = document.createElement('li')
+            li.innerText = msg
+            ul.appendChild(li)
+        }
+    }
+}
+
+const deleteLis = () => {
+    while (ul.firstChild) {
+        ul.firstChild.remove()
+    }
+}
+
+window.addEventListener('DOMContentLoaded', () => {
+    // dog images
+    axios.get(imgUrl).then(resp => setImgs(resp.data.message))
+
+    // dog breeds
+    axios.get(breedUrl).then(resp => setLis(resp.data.message))
+
+    ul.addEventListener('mouseover', (e) => {
+        e.target.style.cursor = 'pointer'
+    })
+
+    ul.addEventListener('click', (e) => {
+        if (e.target.style.color !== "crimson") {
+            e.target.style.color = "crimson"
+        } else {
+            e.target.style.color = 'black'
+        }
+    })
+
+    select.addEventListener('change', (e) => {
+        deleteLis()
+        axios.get(breedUrl).then(resp => setLis(resp.data.message, e.target.value))
+    })
+})
